294e76096962a820f73fa820f1540c6526b5b021
Momose Ryoya <au09084327117@gmail.com>
2017-02-23 04:02:47 +0000

    コメントテーブルのカラム名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220074535) do
+ActiveRecord::Schema.define(version: 20170223034404) do
 
   create_table "comments", force: :cascade do |t|
-    t.text     "title"
-    t.text     "content"
+    t.text     "commenter"
+    t.text     "ccontent"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
